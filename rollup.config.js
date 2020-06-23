e976e917041406a4bc928149c6211dcde1d8b008
RoXoM <xyy94813@sina.com>
2020-06-23 23:45:33 +0800

    chore: minify js by use terser.
    minify js by use terser.
    mjs can distinguish the environment.
    production environment will use the minify module.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { terser } = require('rollup-plugin-terser');
 
 const pkg = require('./package.json');
 
@@ -6,21 +7,47 @@ const deps = [...Object.keys(pkg.peerDependencies || {}), ...Object.keys(pkg.dep
 
 const external = id => deps.includes(id) || /@babel\/runtime\//.test(id);
 
+const inputPath = path.join(__dirname, 'src', 'index.js');
+
 const getBasicConf = () => ({
-  input: path.join(__dirname, 'src', 'index.js'),
+  input: inputPath,
   external,
 });
 
-const getCJSConf = () => ({
-  ...getBasicConf(),
-  output: { file: pkg.main, format: 'cjs' },
-  plugins: [],
-});
+const getDistLib = moduleType => {
+  switch (moduleType) {
+    case 'cjs':
+      return 'lib';
+    case 'es':
+      return 'es';
+    default:
+      throw Error('Unknown Module Type');
+  }
+};
 
-const getEJSConf = () => ({
-  ...getBasicConf(),
-  output: { file: pkg.module, format: 'es' },
-  plugins: [],
-});
+const getConf = (env, moduleType) => {
+  const distLib = getDistLib(moduleType);
+  const fileName = `${pkg.name}.${env}.js`;
+  const outputPath = path.join(distLib, fileName);
+  const isProduction = env === 'production';
+  const plugins = [];
 
-module.exports = [getCJSConf(), getEJSConf()];
+  if (isProduction) {
+    plugins.push(terser());
+  }
+
+  return {
+    ...getBasicConf(),
+    output: {
+      file: outputPath,
+      format: moduleType,
+    },
+    plugins,
+  };
+};
+
+module.exports = [
+  getConf('development', 'cjs'),
+  getConf('production', 'cjs'),
+  getConf('development', 'es'),
+];
